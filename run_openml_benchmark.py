0ca0664d391c2ca6e740d66fe176a56b39abce2f
Matthias_Urban <urbanm@tf.uni-freiburg.de>
2019-02-25 15:15:45 +0100

    tempfile container fix

--- run_openml_benchmark.py
+++ run_openml_benchmark.py
@@ -8,6 +8,7 @@ import sklearn.metrics
 from data_management.data_manager import DataManager, deterministic_shuffle_and_split
 import argparse
 from autosklearn.metrics import balanced_accuracy
+import tempfile
 
 import autosklearn.classification
 
@@ -20,8 +21,8 @@ def main(dataset_name, dataset_id):
         time_left_for_this_task=92400, # sec., how long should this seed fit process run
         per_run_time_limit=6000, # sec., each model may only take this long before it's killed
         ml_memory_limit=8000, # MB, memory limit imposed on each call to a ML algorithm
-        tmp_folder=os.path.join(os.environ["TMPDIR"], "tmp"),
-        output_folder=os.path.join(environ["TMPDIR"], "output"),
+        tmp_folder=os.path.join(tempfile.gettempdir(), "tmp"),
+        output_folder=os.path.join(tempfile.gettempdir(), "output"),
         delete_tmp_folder_after_terminate=False,
         delete_output_folder_after_terminate=False,
         ensemble_size=50,
@@ -38,7 +39,8 @@ def main(dataset_name, dataset_id):
 if __name__ == '__main__':
     dataset_id = int(sys.argv[-1]) - 1
 
-    os.environ['THEANO_FLAGS'] = os.path.join(os.environ["TMPDIR"], "compile")
+    print(tempfile.gettempdir())
+    os.environ['THEANO_FLAGS'] = os.path.join(tempfile.gettempdir(), "compile")
     os.environ['OPENBLAS_NUM_THREADS'] = "1"
 
     with open("openml_datasets.txt", "r") as f:
